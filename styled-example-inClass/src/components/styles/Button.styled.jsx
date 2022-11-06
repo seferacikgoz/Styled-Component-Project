89093126ce246bcf47bcad508dbeee553928c52c
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-07 02:08:11 +0300

    btnstyle

--- styled-example-inClass/src/components/styles/Button.styled.jsx
+++ styled-example-inClass/src/components/styles/Button.styled.jsx
@@ -4,6 +4,17 @@ const Button = styled.button`
 background: ${({bg}) => bg || "white"};
 color: ${({color}) => color || "white"};
 border: 1px solid #a62440;
+border-radius: 5px;
+box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+padding: 1rem 1.2rem;
+font-size: 1.1rem;
+margin-right: 0.5rem;
+margin: 2rem;
+cursor: pointer;
+&:hover{
+    opacity: 0.8;
+    transform: scale(0.97);
+}
 
 `;
 
