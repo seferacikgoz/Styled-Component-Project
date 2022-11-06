2cf6bdf39fc8fdc2b390877fbc348c183d0bb28e
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-07 02:51:15 +0300

    StyledHeader

--- styled-example-inClass/src/App.js
+++ styled-example-inClass/src/App.js
@@ -1,10 +1,19 @@
 import Header from "./components/Header"
+import { ThemeProvider } from "styled-components";
+const style ={
+  colors:{
+    header:"#fff",
+    body:"#fff",
+    footer:"#8A1C4A"
+  },
+
+}
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={style}>
     <Header/>
-    </>
+    </ThemeProvider>
   );
 }
 
