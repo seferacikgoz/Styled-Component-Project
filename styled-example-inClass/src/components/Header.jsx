569fa346996f38b907fea4d3cdbcf92bd26c1587
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-07 17:03:56 +0300

    İmg Nav Styled

--- styled-example-inClass/src/components/Header.jsx
+++ styled-example-inClass/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import Button from "./styles/Button.styled"
-import StyledHeader from "./styles/Header.styled";
+import StyledHeader, { Logo, Nav } from "./styles/Header.styled";
 
 const Header = () => {
   return (
     <StyledHeader>
+        <Nav>
+        <Logo src="./images/logo.png"/>
+        <div>
         <Button color="#A62440">Apply Courses</Button>
         <Button bg="#A62440">Talk To Adviser</Button>
+        </div>
+        </Nav>
     </StyledHeader>
   )
 }
