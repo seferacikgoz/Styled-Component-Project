c866f0b34898e94cb5c6ede40e0558808e84b7b4
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-10 22:48:56 +0300

    footer div

--- styled-example-inClass/src/components/Footer.jsx
+++ styled-example-inClass/src/components/Footer.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
-import {FaTwitterSquare} from "react-icons/fa"
-import {FaFacebookSquare} from "react-icons/fa"
-import {FaLinkedin} from "react-icons/fa"
+import React from "react";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import FooterDiv from "./styles/Footer.styled";
 
 const Footer = () => {
   return (
-    <div>
-      <ul>
-        <li>1775 Tysons Blvd. 5th Floor. Tysons, VA 22102</li>
-        <li>+1 (571) 360 66 77</li>
-        <li>contact@clarusway.com</li>
-      </ul>
+    <>
+      <FooterDiv>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <ul>
+            <li>1775 Tysons Blvd. 5th Floor. Tysons, VA 22102</li>
+            <li>+1 (571) 360 66 77</li>
+            <li>contact@clarusway.com</li>
+          </ul>
 
-      <ul>
-        <li>About Us</li>
-        <li>What We Do</li>
-        <li>FAQ</li>
-      </ul>
+          <ul>
+            <li>About Us</li>
+            <li>What We Do</li>
+            <li>FAQ</li>
+          </ul>
 
-      <ul>
-        <li>Career</li>
-        <li>Blog</li>
-        <li>Contact Us</li>
-      </ul>
+          <ul>
+            <li>Career</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+          </ul>
 
-      <div>
-        <a href="http:/>/www.twitter.com"><FaTwitterSquare/></a>
-        <a href="http:/>/www.facebook.com"><FaFacebookSquare/></a>
-        <a href="http:/>/www.linkedin.com"><FaLinkedin/></a>
-      </div>
-    </div>
-  )
-}
+          <div>
+            <a href="http:/>/www.twitter.com">
+              <FaTwitterSquare />
+            </a>
+            <a href="http:/>/www.facebook.com">
+              <FaFacebookSquare />
+            </a>
+            <a href="http:/>/www.linkedin.com">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </FooterDiv>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ styled-example-inClass/src/components/styles/Footer.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const FooterDiv = styled.footer`
+  /* ... */
+`;
+
+export default FooterDiv;
