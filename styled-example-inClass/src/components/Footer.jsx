3e3fa3dce8fc63c73981ce6e3456e4b60518454c
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-10 21:33:43 +0300

    footer start

--- styled-example-inClass/src/components/Footer.jsx
+++ styled-example-inClass/src/components/Footer.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <ul>
+        <li>1775 Tysons Blvd. 5th Floor. Tysons, VA 22102</li>
+        <li>+1 (571) 360 66 77</li>
+        <li>contact@clarusway.com</li>
+      </ul>
+
+      <ul>
+        <li>About Us</li>
+        <li>What We Do</li>
+        <li>FAQ</li>
+      </ul>
+
+      <ul>
+        <li>Career</li>
+        <li>Blog</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
   )
 }
 
