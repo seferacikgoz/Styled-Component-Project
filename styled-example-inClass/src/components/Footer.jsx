4dfe472267056ab94bec13efc479d78fcc0e84af
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-11 22:38:21 +0300

    footer flex

--- styled-example-inClass/src/components/Footer.jsx
+++ styled-example-inClass/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import FooterDiv, { FooterContent, FooterLogo } from "./styles/Footer.styled";
+import FooterDiv, { FooterContent, FooterFlex, FooterLogo, SocialIcons } from "./styles/Footer.styled";
 
 
 const Footer = () => {
@@ -13,7 +13,7 @@ const Footer = () => {
         <div>
           <FooterLogo src="./images/logo_white.png" />
         </div>
-        <div>
+        <FooterFlex>
           <ul>
             <li>1775 Tysons Blvd. 5th Floor. Tysons, VA 22102</li>
             <li>+1 (571) 360 66 77</li>
@@ -32,7 +32,7 @@ const Footer = () => {
             <li>Contact Us</li>
           </ul>
 
-          <div>
+          <SocialIcons>
             <a href="http:/>/www.twitter.com">
               <FaTwitterSquare />
             </a>
@@ -42,8 +42,8 @@ const Footer = () => {
             <a href="http:/>/www.linkedin.com">
               <FaLinkedin />
             </a>
-          </div>
-        </div>
+          </SocialIcons>
+        </FooterFlex>
         </FooterContent>
       </FooterDiv>
     </>
--- styled-example-inClass/src/components/styles/Footer.styled.jsx
+++ styled-example-inClass/src/components/styles/Footer.styled.jsx
@@ -19,7 +19,43 @@ margin: auto;
 export const FooterLogo = styled.img`
   width: 320px;
   margin: 1rem auto;
+  @media (max-width: ${({theme}) => theme.responsive}) {
+    display: block;
+    width: 90%;
+  }
 `;
+
+
+
+export const FooterFlex = styled.div`
+ display: flex;
+ justify-content: space-between;
+ ul{
+    list-style-type: none;
+ }
+ li{
+    margin-bottom: 1rem;
+    padding: 0 0.4rem;
+ }
+ @media (max-width: ${({theme}) => theme.responsive}) {
+    flex-direction: column;
+    text-align: center;
+    ul{
+        padding: 1.8rem;
+    }
+  }
+`;
+
+
+export const SocialIcons = styled.div`
+ 
+`;
+
+
+
+
+
+
 
 
 
